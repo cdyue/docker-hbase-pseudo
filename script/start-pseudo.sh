ce6e328adf8773ba1b228e8f99682fed245fde66
yu.zhao <nido.zhao@gmail.com>
2017-05-16 00:59:33 +0900

    able to use hbase shell

--- script/start-pseudo.sh
+++ script/start-pseudo.sh
@@ -1,4 +1,5 @@
+#!/bin/sh
+
 ${HBASE_HOME}/bin/hbase-daemon.sh start zookeeper
 ${HBASE_HOME}/bin/hbase-daemon.sh start regionserver
 ${HBASE_HOME}/bin/hbase-daemon.sh start master
-tail -f ${HBASE_HOME}/logs/hbase--master-hbasedocker.log
