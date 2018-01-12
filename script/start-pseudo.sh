2b14a980d77913b2b1add43bc39b6ba2d17c719a
chendy <dengyue.chen@ebaotech.com>
2018-01-12 17:49:00 +0800

    initialize hbase if   /data/init-result does not exist.

--- script/start-pseudo.sh
+++ script/start-pseudo.sh
@@ -3,4 +3,11 @@
 ${HBASE_HOME}/bin/hbase-daemon.sh start zookeeper
 ${HBASE_HOME}/bin/hbase-daemon.sh start regionserver
 ${HBASE_HOME}/bin/hbase-daemon.sh start master
+
+if [[ ! -f /data/init-result ]]
+then
+  echo "sleep 30s and initialization..."
+  sleep 30
+  $HBASE_HOME/bin/hbase shell /data/init-hbase.txt > /data/init-result
+fi
 tail -f /hbase-1.2.0/logs/*.out
