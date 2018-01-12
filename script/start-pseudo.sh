b6b083e2f96cfcdb9db580382e5c9cc1a50c67c4
chendy <dengyue.chen@ebaotech.com>
2018-01-12 16:33:00 +0800

    no message

--- script/start-pseudo.sh
+++ script/start-pseudo.sh
@@ -3,3 +3,4 @@
 ${HBASE_HOME}/bin/hbase-daemon.sh start zookeeper
 ${HBASE_HOME}/bin/hbase-daemon.sh start regionserver
 ${HBASE_HOME}/bin/hbase-daemon.sh start master
+tail -f /hbase-1.2.0/logs/*.out
